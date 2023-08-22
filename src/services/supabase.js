ab39d5897e4fae1196366d042f03e2516af6787c
HadleyStephanie <lynnhadley92@gmail.com>
2023-08-22 14:55:06 -0700

    Database connection and React Query cache created

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://tzjdnovwixteyjxkldub.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR6amRub3Z3aXh0ZXlqeGtsZHViIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTI2NTczNzUsImV4cCI6MjAwODIzMzM3NX0.o3U1PlUUbGFvhArgRPwQWd05JOUgVtGee8gUzVosNxA";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
